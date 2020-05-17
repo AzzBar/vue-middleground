aeca28b0c78856077bb794ce9d6632cc04b7cbcc
AzzBar <44644802+AzzBar@users.noreply.github.com>
2020-05-17 13:49:11 -0400

    lint formatting

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
     <GmapMap
-  :center="{lat:10, lng:10}"
-  :zoom="7"
-  map-type-id="terrain"
-  style="width: 500px; height: 300px"
->
-  <GmapMarker
-    :key="index"
-    v-for="(m, index) in markers"
-    :position="m.position"
-    :clickable="true"
-    :draggable="true"
-    @click="center=m.position"
-  />
-</GmapMap>
+      :center="{ lat: 10, lng: 10 }"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      />
+    </GmapMap>
   </div>
 </template>
 
@@ -25,19 +25,15 @@ export default {
       center: { lat: 10.0, lng: 10.0 },
       markers: [
         {
-          position: { lat: 10.0, lng: 10.1}
+          position: { lat: 10.0, lng: 10.1 }
         },
         {
-          position: { lat: 12.0, lng: 12.0}
+          position: { lat: 12.0, lng: 12.0 }
         }
       ]
-
-    }
+    };
   }
-  
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ import * as VueGoogleMaps from "vue2-google-maps";
 require("dotenv").config();
 
 Vue.use(VueGoogleMaps, {
-    load: { 
-      key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
-      libraries: "places"
-    }
-  });
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
+    libraries: "places"
+  }
+});
 
 Vue.config.productionTip = false;
 
