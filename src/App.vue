8558b93e4cdda3413e18068ec349018b3fa7b861
AzzBar <44644802+AzzBar@users.noreply.github.com>
2020-05-17 13:11:45 -0400

    first

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <GmapMap
+  :center="{lat:10, lng:10}"
+  :zoom="7"
+  map-type-id="terrain"
+  style="width: 500px; height: 300px"
+>
+  <GmapMarker
+    :key="index"
+    v-for="(m, index) in markers"
+    :position="m.position"
+    :clickable="true"
+    :draggable="true"
+    @click="center=m.position"
+  />
+</GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      center: { lat: 10.0, lng: 10.0 },
+      markers: [
+        {
+          position: { lat: 10.0, lng: 10.1}
+        },
+        {
+          position: { lat: 12.0, lng: 12.0}
+        }
+      ]
+
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import * as VueGoogleMaps from "vue2-google-maps";
+
+require("dotenv").config();
+
+Vue.use(VueGoogleMaps, {
+    load: { 
+      key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
+      libraries: "places"
+    }
+  });
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
